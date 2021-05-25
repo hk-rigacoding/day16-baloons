32901136873b910b90dda207917aef95b022a8c2
hk-rigacoding <hk.rigacoding@gmail.com>
2021-05-25 21:23:14 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace day16_baloons
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.baloonsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eJPRomToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nācAtpakaļToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Survivors = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Hits = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.baloonsToolStripMenuItem,
+            this.eJPRomToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1072, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // baloonsToolStripMenuItem
+            // 
+            this.baloonsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem});
+            this.baloonsToolStripMenuItem.Name = "baloonsToolStripMenuItem";
+            this.baloonsToolStripMenuItem.Size = new System.Drawing.Size(80, 24);
+            this.baloonsToolStripMenuItem.Text = "Baloons!";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(123, 26);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // eJPRomToolStripMenuItem
+            // 
+            this.eJPRomToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nācAtpakaļToolStripMenuItem});
+            this.eJPRomToolStripMenuItem.Name = "eJPRomToolStripMenuItem";
+            this.eJPRomToolStripMenuItem.Size = new System.Drawing.Size(87, 24);
+            this.eJPRomToolStripMenuItem.Text = "EJ PRom !";
+            this.eJPRomToolStripMenuItem.Click += new System.EventHandler(this.eJPRomToolStripMenuItem_Click);
+            // 
+            // nācAtpakaļToolStripMenuItem
+            // 
+            this.nācAtpakaļToolStripMenuItem.Name = "nācAtpakaļToolStripMenuItem";
+            this.nācAtpakaļToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.nācAtpakaļToolStripMenuItem.Text = "Nāc Atpakaļ !";
+            this.nācAtpakaļToolStripMenuItem.Click += new System.EventHandler(this.nācAtpakaļToolStripMenuItem_Click);
+            // 
+            // Survivors
+            // 
+            this.Survivors.AutoSize = true;
+            this.Survivors.Location = new System.Drawing.Point(346, 10);
+            this.Survivors.Name = "Survivors";
+            this.Survivors.Size = new System.Drawing.Size(75, 17);
+            this.Survivors.TabIndex = 1;
+            this.Survivors.Text = "Survivors :";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Hits
+            // 
+            this.Hits.AutoSize = true;
+            this.Hits.Location = new System.Drawing.Point(447, 9);
+            this.Hits.Name = "Hits";
+            this.Hits.Size = new System.Drawing.Size(40, 17);
+            this.Hits.TabIndex = 2;
+            this.Hits.Text = "Hits :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1072, 574);
+            this.Controls.Add(this.Hits);
+            this.Controls.Add(this.Survivors);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Baloons !";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem baloonsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eJPRomToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nācAtpakaļToolStripMenuItem;
+        private System.Windows.Forms.Label Survivors;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label Hits;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace day16_baloons
+{
+
+
+    public partial class Form1 : Form
+    {
+        baloon[] b_mas;
+        int hits = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //0. jāizvēlās [randoms] skaits balonu
+            //cik liels būs masīva izmērs ?
+
+
+            int b_skaits = this.ClientRectangle.Width / Properties.Resources.red.Width;
+            Random r = new Random();
+            int sk = r.Next(1, b_skaits - 1);
+
+
+            b_mas = new baloon[sk];
+
+            int offset = 0;
+
+            for (int i=0; i< b_mas.Length; i++)
+            {
+                int kr = r.Next(1,4);
+
+                Bitmap image = null;
+ 
+                switch (kr)
+                {
+                    case 1:
+                        image = Properties.Resources.blue;
+                        break;
+                    case 2:
+                        image = Properties.Resources.red;
+                        break;
+                    case 3:
+                        image = Properties.Resources.yellow;
+                        break;
+                }
+
+
+                //ko vēl vajag ?
+                //vajag visu sarēķināt un instancēt
+                // baloon(int aatrums, int izmX, int izmY, int saakX, int saakY, Bitmap bilde, int max)
+
+
+                b_mas[i] = new baloon(r.Next(1, 10), image.Width, image.Height, offset , 10, image, this.ClientRectangle.Height);
+                offset += image.Width + 10;
+                this.Controls.Add(b_mas[i]);
+
+            }
+
+
+
+            timer1.Start();
+
+
+
+
+
+
+
+
+
+        }
+
+        private void eJPRomToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nācAtpakaļToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int survivors = 0;
+            
+            
+            //foreach (baloon b in b_mas)
+            for (int i=0; i< b_mas.Length; i++)
+            {
+                if (b_mas[i] == null)
+                    continue;
+
+                survivors += b_mas[i].survived;
+                if (b_mas[i].shot)
+                {
+                    hits++;
+                    b_mas[i] = null;
+                }
+
+            }
+
+            Survivors.Text = "Survivors : " + survivors;
+            Hits.Text = "Hits : " + hits;
+        }
+    }
+
+
+    class baloon : PictureBox
+    {
+        public int survived;
+        public bool shot;
+
+        //kam klasē jābūt ?
+        //ātrums : kā mēs ātrumu rēķināsim ?
+        int speed;
+        int sizeX;
+        int sizeY;
+        int startX;
+        int startY;
+        int maxY;
+
+
+
+        //šis katras 50ms atjauninās balona pozīciju
+        Timer t;
+
+        //vai vajag bitmapa lauku ?
+
+        public baloon (int aatrums, int izmX, int izmY, int saakX, int saakY, Bitmap bilde, int max)
+        {
+
+            //System.Media.SoundPlayer sp = new System.Media.SoundPlayer("fails.mp3");
+
+            //kautkas jādara
+            shot = false;
+            survived = 0;
+            maxY = max;
+            speed = aatrums;
+            sizeX = izmX;
+            sizeY = izmY;
+            startX = saakX;
+            startY = saakY;
+            this.Image = bilde;
+            this.Location = new Point(startX, startY);
+
+            //šo neuzstādot bildes ir izkropļotas !
+            this.Size = new Size(sizeX, sizeY);
+            this.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            this.Click += Baloon_Click;
+
+            t = new Timer();
+
+            t.Interval = 50;
+            t.Tick += T_Tick;
+            t.Start();
+        }
+
+        private void Baloon_Click(object sender, EventArgs e)
+        {
+            //atskaņot skaņu !!!
+            shot = true;
+            t.Stop();
+            this.Dispose();
+        }
+
+        private void T_Tick(object sender, EventArgs e)
+        {
+            //ko šeit jādara ?
+            this.Location = new Point(this.Location.X, this.Location.Y + speed);
+            
+            Application.DoEvents();//maybe neobligāts, jo taimeri ir trīdi !!!
+
+            //pārbaudīt vai nav ārpus ekrāna !!!
+
+            if (this.Location.Y > maxY)
+            {
+                //t.Stop();
+                //this.Dispose();
+                this.Location = new Point(startX, startY);
+                survived++;
+            }
+
+
+        }
+    }
+}
